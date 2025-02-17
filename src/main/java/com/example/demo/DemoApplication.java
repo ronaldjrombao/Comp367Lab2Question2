5ce3977fccf4432b2f22916b40bc854a0ed8610e
Ronald Ombao <ronaldjr.ombao@gmail.com>
2025-02-17 16:28:29 -0500

    Modify Message for depending on time of day This fixes #1.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalTime;
 
 @SpringBootApplication
 @Controller
@@ -14,8 +18,19 @@ public class DemoApplication {
     }
 
     @GetMapping("/")
-    public String index() {
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "John") String name,
+                           Model model) {
+        LocalTime now = LocalTime.now();
+        int hour = now.getHour();
+        String greetingMessage;
+
+        if (hour < 12) {
+            greetingMessage = "Good morning, " + name + ", Welcome to COMP367";
+        } else {
+            greetingMessage = "Good afternoon, " + name + ", Welcome to COMP367";
+        }
+
+        model.addAttribute("greeting", greetingMessage);
         return "index";
     }
-
 }
